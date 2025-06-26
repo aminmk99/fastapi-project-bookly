22d3008e4a6c5b8d3dbff3a19d996ca2693a03ae
Amin Mirzakuchaki <amirzakuchaki@gmail.com>
2025-06-26 17:17:02 +0330

    Add database model

---
+++ src/books/models.py
@@ -0,0 +1,24 @@
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+from sqlalchemy.sql import func
+from datetime import datetime
+import uuid
+
+
+class Book(SQLModel, table=True):
+    __tablename__ = "books"
+
+    uid: uuid.UUID = Field(
+        sa_column=Column(pg.UUID, nullable=False, primary_key=True, default=uuid.uuid4)
+    )
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, server_default=func.now()))
+    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    
+    def __repr__(self):
+        return f"<Book {self.title}>"

caeab5346c0e7aabb1271760a46a603d8975dce7
Amin Mirzakuchaki <amirzakuchaki@gmail.com>
2025-07-03 15:42:50 +0330

    Implement get_session

--- src/db/main.py
+++ src/db/main.py
@@ -1,5 +1,7 @@
 from sqlmodel import create_engine, text, SQLModel
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
 from src.config import Config
 
 """
@@ -8,14 +10,24 @@ Don't forget to check for his answer.
 engine = AsyncEngine(create_engine(url=Config.DATABASE_URL, echo=True))
 """
 
-#The way chatgpt suggests:
+# The way chatgpt suggests:
 engine = create_async_engine(Config.DATABASE_URL, echo=True)
+
 
 async def init_db():
     async with engine.begin() as conn:
         from src.books.models import Book
-        
+
         await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def get_session() -> AsyncSession:
+
+    Session = sessionmaker(
+        bind=engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+    )
         
-async def get_session():
-    pass
+    async with Session() as session:
+        yield session

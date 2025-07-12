cc10201f2c56cd8596fa2eb825eb4c5121d52bf4
Amin Mirzakuchaki <amirzakuchaki@gmail.com>
2025-07-12 23:59:12 +0330

    Add changes

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, status, Depends
 from fastapi.exceptions import HTTPException
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-from .schemas import UserCreate
+from .schemas import UserCreate, User
 from .service import UserService
 from src.db.main import get_session
 
@@ -10,7 +10,11 @@ auth_router = APIRouter()
 user_service = UserService()
 
 
-@auth_router.post("/signup")
+@auth_router.post(
+    "/signup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+)
 async def create_user_account(
     user_data: UserCreate, session: AsyncSession = Depends(get_session)
 ):
@@ -23,6 +27,6 @@ async def create_user_account(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="User with this email already exists",
         )
-    new_user = await user_service.user_create(user_data)
-    
+    new_user = await user_service.user_create(user_data, session)
+
     return new_user
--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -4,6 +4,8 @@ import uuid
 
 
 class UserCreate(BaseModel):
+    first_name: str = Field(max_length=25)
+    last_name: str = Field(max_length=25)
     username: str = Field(max_length=8)
     email: str = Field(max_length=40)
     password: str = Field(min_length=6)

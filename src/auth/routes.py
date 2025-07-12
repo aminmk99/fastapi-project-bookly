4c03ab89a596895971caa83ca833b3b793aee5c6
Amin Mirzakuchaki <amirzakuchaki@gmail.com>
2025-07-12 20:56:42 +0330

    created service.py for auth module

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
+from .schemas import UserCreate
 
 auth_router = APIRouter()
 
 @auth_router.post('/signup')
-async def create_user_account():
+async def create_user_account(user_data: UserCreate):
     pass

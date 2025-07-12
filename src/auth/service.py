b1ea514cc8428a7a2e904caf83308b4331fc0566
Amin Mirzakuchaki <amirzakuchaki@gmail.com>
2025-07-12 22:54:15 +0330

    Add user_create in UserService

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,5 +1,6 @@
 from .models import User
 from .schemas import UserCreate
+from .utils import generate_passwd_hash
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlmodel import select
 
@@ -23,4 +24,23 @@ class UserService:
     async def user_create(self, user_data: UserCreate, session: AsyncSession):
         user_data_dict = user_data.model_dump()
         
+        new_user = User(**user_data)
+        
+        new_user.password_hash = generate_passwd_hash(user_data_dict['password'])
+        
+        session.add(new_user)
+        
+        await session.commit()
+        
+        return new_user
+        
+        
+        """
+        chatgpt style:
+        user_data_dict['password_hash'] = generate_password_hash(user_data_dict['password'])
+
+        # Optionally delete plain password
+        del user_data_dict['password']
+
         new_user = User(**user_data_dict)
+        """

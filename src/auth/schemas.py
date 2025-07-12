e4b69f14831dd265ed94c9f7148d752464ef088b
Amin Mirzakuchaki <amirzakuchaki@gmail.com>
2025-07-12 23:18:11 +0330

    Add User Schema

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,7 +1,21 @@
 from pydantic import BaseModel, Field
+from datetime import datetime
+import uuid
+
 
 class UserCreate(BaseModel):
     username: str = Field(max_length=8)
     email: str = Field(max_length=40)
     password: str = Field(min_length=6)
-    
+
+
+class User(BaseModel):
+    uid: uuid.UUID
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    is_verified: bool
+    password_hash: str = Field(exclude=True)
+    created_at: datetime
+    updated_at: datetime

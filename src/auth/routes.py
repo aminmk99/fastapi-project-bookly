721f459898f31b266f7a8611bb483424c229094b
Amin Mirzakuchaki <amirzakuchaki@gmail.com>
2025-07-21 21:35:06 +0330

    Complete Login path handler

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,14 +1,18 @@
 from fastapi import APIRouter, status, Depends
 from fastapi.exceptions import HTTPException
+from fastapi.responses import JSONResponse
 from sqlmodel.ext.asyncio.session import AsyncSession
+from datetime import timedelta
 
-from .schemas import UserCreate, User
+from .schemas import UserCreate, User, UserLogin
 from .service import UserService
-from .utils import create_access_token, decode_token
+from .utils import create_access_token, decode_token, verify_password
 from src.db.main import get_session
 
 auth_router = APIRouter()
 user_service = UserService()
+
+REFRESH_TOKEN_EXPIRY = 2
 
 
 @auth_router.post(
@@ -33,6 +37,70 @@ async def create_user_account(
     return new_user
 
 
-@auth_router.post('/login')
-async def login_users():
-    pass
+@auth_router.post("/login")
+async def login_users(
+    login_data: UserLogin, session: AsyncSession = Depends(get_session)
+):
+    email = login_data.email
+    password = login_data.password
+
+    user = await user_service.get_user_by_email(email, session)
+    
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid email or password"
+        )
+        
+    if not verify_password(password, user.password_hash):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid email or password"
+        )
+        
+    access_token = create_access_token(
+                user_data={"email": user.email, "user uid": str(user.uid)}
+            )
+    refresh_token = create_access_token(
+        user_data={"email": user.email, "user uid": str(user.uid)},
+        expiry=timedelta(days=REFRESH_TOKEN_EXPIRY),
+        refresh=True
+    )
+            
+    return JSONResponse(
+        content={
+            "message": "Login Successful",
+            "access_token": access_token,
+            "refresh_token": refresh_token,
+            "user": {
+                "email": user.email,
+                "uid": str(user.uid)
+            }
+        }
+    )
+
+    # if user is not None:
+    #     is_password_valid = verify_password(password, user.password_hash)
+
+    #     if is_password_valid:
+    #         access_token = create_access_token(
+    #             user_data={"email": user.email, "user uid": str(user.uid)}
+    #         )
+    #         refresh_token = create_access_token(
+    #             user_data={"email": user.email, "user uid": str(user.uid)},
+    #             expiry=timedelta(days=REFRESH_TOKEN_EXPIRY),
+    #             refresh=True
+    #         )
+            
+    #         return JSONResponse(
+    #             content={
+    #                 "message": "Login Successful",
+    #                 "access_token": access_token,
+    #                 "refresh_token": refresh_token,
+    #                 "user": {
+    #                     "email": user.email,
+    #                     "uid": str(user.uid)
+    #                 }
+    #             }
+    #         )
+            

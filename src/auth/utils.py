067396cad1589dcfb42d30dd49a8471f9912e1cf
Amin Mirzakuchaki <amirzakuchaki@gmail.com>
2025-07-21 19:45:37 +0330

    Complete create_access_token

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -2,24 +2,38 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from src.config import Config
 import jwt
+import uuid
 
 passwd_context = CryptContext(schemes=["bcrypt"])
 
+ACCESS_TOKEN_EXPIRY = 3600
+
+
 def generate_passwd_hash(password: str) -> str:
     hash = passwd_context.hash(password)
-    
+
     return hash
+
 
 def verify_password(password: str, hash: str) -> bool:
     return passwd_context.verify(password, hash)
 
-def create_access_token(user_data: dict, expiry: timedelta):
+
+def create_access_token(
+    user_data: dict, expiry: timedelta = None, refresh: bool = False
+):
     payload = {}
-    
-    token= jwt.encode(
-        payload=payload,
-        key=Config.JWT_SECRET,
-        algorithm=Config.JWT_ALGORITHM
+
+    payload["user"] = user_data
+    payload["exp"] = datetime.now() + (
+        expiry if expiry is not None else timedelta(seconds=ACCESS_TOKEN_EXPIRY)
     )
-    
+    # ChatGPT and Copilot suggests datetime.utcnow() instead of datetime.now()
+    payload["jti"] = str(uuid.uuid4())
+    payload['refresh'] = refresh
+
+    token = jwt.encode(
+        payload=payload, key=Config.JWT_SECRET, algorithm=Config.JWT_ALGORITHM
+    )
+
     return token

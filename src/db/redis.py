b4c0cdadef033902f66808cf566f606300b8b6c1
Amin Mirzakuchaki <amirzakuchaki@gmail.com>
2025-07-31 15:11:57 +0330

    Implement function for add token id to block list

--- src/db/redis.py
+++ src/db/redis.py
@@ -1,6 +1,12 @@
 import aioredis
 from src.config import Config
 
+JTI_EXPIRY = 3600
+
 token_block_list = aioredis.StrictRedis(
     host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=0
 )
+
+
+async def add_jwi_to_blocklist(jti: str) -> None:
+    await token_block_list.set(name=jti, value="", ex=JTI_EXPIRY)
